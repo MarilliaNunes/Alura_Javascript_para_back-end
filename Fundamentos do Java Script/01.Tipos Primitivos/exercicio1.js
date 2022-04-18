90154bfdd832d2a56d64803914e9519224940fec
MarilliaVasconcelos <mvasconcelos@tractian.com>
2022-04-17 23:06:46 -0300

    ordenacao das pastas

---
+++ Fundamentos do Java Script/01.Tipos Primitivos/exercicio1.js
@@ -0,0 +1,9 @@
+const idade = 2
+const nome = "Lara"
+const sobrenome = "Ayumi"
+const bacharel = false
+const dezAnosDepois = "12"
+
+console.log(nome+sobrenome)
+console.log(idade+dezAnosDepois)
+console.log(bacharel==1)
