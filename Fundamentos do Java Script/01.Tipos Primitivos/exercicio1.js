7d7c3087d839ecce805ccd345541d15e323fd30c
MarilliaVasconcelos <mvasconcelos@tractian.com>
2022-04-17 23:24:39 -0300

    new

--- Fundamentos do Java Script/01.Tipos Primitivos/exercicio1.js
+++
@@ -1,9 +0,0 @@
-const idade = 2
-const nome = "Lara"
-const sobrenome = "Ayumi"
-const bacharel = false
-const dezAnosDepois = "12"
-
-console.log(nome+sobrenome)
-console.log(idade+dezAnosDepois)
-console.log(bacharel==1)
