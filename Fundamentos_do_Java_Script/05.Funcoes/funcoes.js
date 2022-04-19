70a5fa436fe3e26aa88711a36e12f9566cb1bdd1
MarilliaVasconcelos <mvasconcelos@tractian.com>
2022-04-19 00:21:16 -0300

    the end aulas

--- Fundamentos_do_Java_Script/05.Funcoes/funcoes.js
+++ Fundamentos_do_Java_Script/05.Funcoes/funcoes.js
@@ -6,4 +6,20 @@ function funcao(parametros = 'valor padrão'){ //podemos definir um valor padrã
     return resposta
 }
 
-console.log(funcao('resposta'))
+//Expressão de Função
+
+const soma = function (num1, num2){return num1+num2} //funções anônimas
+
+// Diferença entre Declaração e Expressão de função
+// --> A expressão de função tem que ser declarada antes de ser chamada, pois ela se comporta com uma variavel
+// --> funções declaradas e var são listadas antes da execução do arquivo
+
+//Arrow Function
+// Facilidade para tratar com objetos
+// Hoisting: arrow function se comporta como expressão
+
+const apresentarArrow = nome => `meu nome é ${nome}`
+
+const sum = (num1,num2) => num1+num2
+
+console.log(sum(2,4))
